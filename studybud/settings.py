2910879cda3e61ef96f46166fa30e8bf91a918e8
Vishal Kumar <86667753+vishalKumar1311@users.noreply.github.com>
2023-12-08 01:39:59 +0530

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangodb',
+        'USER': 'mysuperuser',
+        'PASSWORD' : 'mysuperuser',
+        'HOST':'djangodb.cw5yrhobys6j.us-east-1.rds.amazonaws.com',
+        'PORT':'5432',
+
     }
 }
 
