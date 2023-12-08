85bb56d50550304779fc6493f7731bd8ab44d519
Vishal Kumar <86667753+vishalKumar1311@users.noreply.github.com>
2023-12-08 14:44:21 +0530

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['67.202.14.199', 'localhost', '127.0.0.1']
 
 
 # Application definition
